fa396472f5a1b4d8e7a0ecf2eee960ded831829f
Anelya <amangazyy@MacBook-Air--Anelya.local>
2025-07-31 15:11:33 +0500

    Добавила все файлы проекта

---
+++ script.js
@@ -0,0 +1,14 @@
+// function login() {
+//   const login = document.getElementById("login").value;
+//   const password = document.getElementById("password").value;
+//   if (login === "admin" && password === "1234") {
+//     document.querySelector(".login-box").classList.add("hidden");
+//     document.querySelector(".birthday-box").classList.remove("hidden");
+//   } else {
+//     alert("Неправильный логин или пароль");
+//   }
+// }
+
+// function showBirthday() {
+//   alert("Введите свою дату рождения в поле ниже");
+// }
